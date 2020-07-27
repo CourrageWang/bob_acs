1fc09ec80b702e086ee49eb1e82d701c07d7157f
CourrageWang <wangyongqi_46@163.com>
2020-07-27 10:56:02 +0800

    1、添加按照日期备份打卡数据。

--- src/main/java/com/irain/Main.java
+++ src/main/java/com/irain/Main.java
@@ -1,7 +1,8 @@
 package com.irain;
 
 import com.irain.conf.LoadConf;
-import com.irain.task.TimeTask;
+import com.irain.handle.DeviceInfo;
+import com.irain.utils.TimeUtils;
 import lombok.extern.log4j.Log4j;
 
 /**
@@ -14,16 +15,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        log.info("---------------程序开始执行-----------------");
-        //加载配置文件
-        new LoadConf();
-        //每天的凌晨1点开始执行定时任务完成前一日考勤数据导入;
-        new TimeTask().dayOfLoadSignData("01:00:10");
-
-        //每一个小时监测所有设备连接是否正常并同时执行校时操作【每小时执行一次监测程序】；
-        new TimeTask().checkConnection();
-
-        //每天凌晨四点开始将所有打卡机上的数据同步，并通过增量的方式存储在指定的excel文件夹下；
-        new TimeTask().dayOfLoadAllDeviceData("03:00:00");
+        String loadData = "";
+        if (args.length > 0) {
+            loadData = args[1];
+            if (!TimeUtils.isValidDate(loadData, "YYYYMMdd") || loadData.length() != 8) {
+                log.info("******************************");
+                log.info("*您输入的时间格式错误，请重新输入*");
+                log.info("*  输入格式形如20200412的类型   *");
+                log.info("*******************************");
+            } else {
+                log.info("数据格式正确程序即将录入" + loadData + "的数据！！");
+                log.info("---------------程序开始执行-----------------");
+                //加载配置文件
+                new LoadConf();
+                //开始处理输入数据 日期为需要导入数据的日期
+                // 如导入20200701的数据 则输入日需要重新导入的日期。
+                new DeviceInfo().loadAllDeviceData(LoadConf.devicesMap, loadData);
+                log.info("******获取考勤数据定时任务执行结束*******");
+            }
+        } else {
+            System.out.println("您输入的参数为空 请重新输入！！！");
+        }
     }
 }
